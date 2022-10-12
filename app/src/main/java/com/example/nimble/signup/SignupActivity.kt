4d86fb169735c464c48ff329db653803f1f49044
DeMark <yanchukdima2003@gmail.com>
2022-10-12 23:03:32 +0300

    feat: New registration

---
+++ app/src/main/java/com/example/nimble/signup/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nimble.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nimble.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
