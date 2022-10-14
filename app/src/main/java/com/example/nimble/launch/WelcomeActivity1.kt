289caced28f8fd4f4adf6fb3adada17bcc08c8a6
ShootkaxD <kokasuxin@gmail.com>
2022-10-14 12:02:28 +0300

    Add LaunchActivity

--- app/src/main/java/com/example/nimble/launch/WelcomeActivity1.kt
+++ app/src/main/java/com/example/nimble/launch/WelcomeActivity1.kt
@@ -1,8 +1,6 @@
 package com.example.nimble.launch
 
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,30 +9,13 @@ import com.example.nimble.R
 
 
 class WelcomeActivity1 : AppCompatActivity() {
-
-    lateinit var sp: SharedPreferences
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome1)
 
-        sp = getSharedPreferences("APP_SETTINGS", Context.MODE_PRIVATE)
-        start()
     }
 
-    fun start(){
-        val flag = sp.getBoolean("flag", false)
-        if(!flag){
-            val editor = sp.edit()
-            editor.putBoolean("flag", true)
-            editor.apply()
-            val intent = Intent(this, WelcomeActivity1::class.java)
-            startActivity(intent)
-        }else{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-    }
+
 
     fun next(view: View) {
 
