73b7da0d6da337cf7d1e00cb1029a689e1811aab
ShootkaXd <kokasuxin@gmail.com>
2022-10-16 02:22:13 +0300

    add ActionBar to MainActivity

--- app/src/main/java/com/example/nimble/launch/MainActivity.kt
+++ app/src/main/java/com/example/nimble/launch/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.nimble.launch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.nimble.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
+
 }
