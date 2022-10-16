c2424fbb4137f438e309b2292f1e940abc9f0921
ShootkaXd <kokasuxin@gmail.com>
2022-10-16 17:34:06 +0300

    add Search_bar

--- app/src/main/java/com/example/nimble/signin/SigninActivity.kt
+++ app/src/main/java/com/example/nimble/signin/SigninActivity.kt
@@ -1,11 +1,8 @@
 package com.example.nimble.signin
 
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import com.example.nimble.R
 import com.example.nimble.signup.SignupActivity
